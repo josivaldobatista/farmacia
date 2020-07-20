a203c7e794a08d82b9e258a4fb1695f340f0e574
Josivaldo Batista <josivaldo.fb@gmail.com>
2020-07-20 10:54:41 -0300

    Atualização: utilizando apenas @JsonIgnore.

--- src/main/java/com/farmacia/domains/Pedido.java
+++ src/main/java/com/farmacia/domains/Pedido.java
@@ -17,7 +17,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable {
@@ -32,11 +31,9 @@ public class Pedido implements Serializable {
 
   // Essa anotação é necessário para não dar erro de entidade transiente
   // quando for salvar o pedido e o pagamento dele.
-  @JsonManagedReference
   @OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
   private Pagamento pagamento;
 
-  @JsonManagedReference
   @ManyToOne
   @JoinColumn(name = "cliente_id")
   private Cliente cliente;
